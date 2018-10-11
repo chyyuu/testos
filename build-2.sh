bb08c5cb629b504c0ed7a71ca77cd45973f00bca
chyyuu <yuchen@tsinghua.edu.cn>
2018-10-11 20:15:43 +0800

    minimal rust kernel

---
+++ build-2.sh
@@ -0,0 +1,1 @@
+cargo xbuild --target x86_64-blog_os.json
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 #![no_std] // don't link the Rust standard library
 #![no_main] // disable all Rust-level entry points
-
+extern crate bootloader_precompiled;
 use core::panic::PanicInfo;
 
 /// This function is called on panic.
@@ -9,10 +9,18 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
-// On Linux:
-#[no_mangle] // don't mangle the name of this function
+static HELLO: &[u8] = b"Hello World!";
+
+#[no_mangle]
 pub extern "C" fn _start() -> ! {
-    // this function is the entry point, since the linker looks for a function
-    // named `_start` by default
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
     loop {}
 }
