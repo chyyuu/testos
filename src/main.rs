d3e0206ce5e5293c9d4ca1b77bd97a2e6e080b33
chyyuu <yuchen@tsinghua.edu.cn>
2018-10-11 23:52:13 +0800

    step 3: vga-text-mode

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,15 @@
 #![no_std] // don't link the Rust standard library
 #![no_main] // disable all Rust-level entry points
 extern crate bootloader_precompiled;
+extern crate spin;
+extern crate volatile;
+#[macro_use]
+extern crate lazy_static;
+
+pub mod vga_buffer;
+//use vga_buffer::{ColorCode,Color,Writer};
 use core::panic::PanicInfo;
+
 
 /// This function is called on panic.
 #[panic_handler]
@@ -9,18 +17,9 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
-static HELLO: &[u8] = b"Hello World!";
-
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+	vga_buffer::print_something();
 
     loop {}
 }
