aa982c01a91861cf33bc7613eb3ef796a2021048
chyyuu <yuchen@tsinghua.edu.cn>
2018-10-12 11:02:53 +0800

    add double-fault handler

--- src/main.rs
+++ src/main.rs
@@ -20,6 +20,13 @@ pub extern "C" fn _start() -> ! {
     init_idt();
     // invoke a breakpoint exception
     x86_64::instructions::int3();
+
+    // trigger a page fault
+    unsafe {
+        *(0xdeadbeef as *mut u64) = 42;
+    };
+
+
     println!("It did not crash!");
 
     //unsafe { testos::exit_qemu(); }
@@ -44,6 +51,7 @@ lazy_static! {
     static ref IDT: InterruptDescriptorTable = {
         let mut idt = InterruptDescriptorTable::new();
         idt.breakpoint.set_handler_fn(breakpoint_handler);
+        idt.double_fault.set_handler_fn(double_fault_handler); // new
         idt
     };
 }
@@ -58,3 +66,10 @@ extern "x86-interrupt" fn breakpoint_handler(
     println!("EXCEPTION: BREAKPOINT\n{:#?}", stack_frame);
 }
 
+// new
+extern "x86-interrupt" fn double_fault_handler(
+    stack_frame: &mut ExceptionStackFrame, _error_code: u64)
+{
+    println!("EXCEPTION: DOUBLE FAULT\n{:#?}", stack_frame);
+    loop {}
+}
